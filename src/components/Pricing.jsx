e2f491a8cb314021bace71eb46e66c38522e7e37
ayushtenguria <hacktiger.as@gmail.com>
2023-06-26 15:25:58 +0530

    Pricing page started

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,11 +1,18 @@
+import { PricingCard } from "./PricingCard";
+
 export const Pricing = () => {
   return (
-    <section className="p-6 m-4">
-      <div>
-        <h1 className="text-5xl md:text-6xl font-semibold bg-gradient-to-r from-[#193C8F] via-[#9A4A12] to-[#9A4A12] inline-block text-transparent bg-clip-text">
-          Pricing
-        </h1>
-        <p>Lorem ipssum kuch bhi aira gera likhdo for dummy text</p>
+    <section className=" bg-gradient-to-br from-white  via-[#F7B5EA] to-[#5FFBF1]">
+      <div className="p-10 ">
+        <div>
+          <h1 className="text-5xl md:text-6xl font-semibold bg-gradient-to-r from-[#193C8F] via-[#9A4A12] to-[#9A4A12] inline-block text-transparent bg-clip-text">
+            Pricing
+          </h1>
+          <p>Lorem ipssum kuch bhi aira gera likhdo for dummy text</p>
+        </div>
+        <div>
+          <PricingCard />
+        </div>
       </div>
     </section>
   );
---
+++ src/components/PricingCard.jsx
@@ -0,0 +1,10 @@
+export const PricingCard = () => {
+    return(
+        <div className="p-4 border-2 border-[#7C2D12] inline-block">
+            <div><h1 className="text-[#7C2D12] font-semibold decoration-2 underline text-4xl md:text-5xl">Focus</h1></div>
+            <div><p>Some lorem ipsum text that does not have any real meaning here is only jus for show off and fill up the dummy text</p></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
